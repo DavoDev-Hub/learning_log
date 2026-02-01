3d5e91e29d6d060424c007118da54dffeb03f6e0
DavoDev-Hub <juanpajmz13@gmail.com>
2026-01-31 21:42:21 -0600

    Agregar formulario para crear y actualizar entradas, incluyendo etiquetas y widgets personalizados

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -34,3 +34,11 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    """Form for creating and updating Entry instances."""
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
